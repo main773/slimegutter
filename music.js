aa7564f18ec05fdd87d27f3fcd65edc64764efaa
main773 <chatterapp1234@gmail.com>
2025-10-03 23:13:43 -0400

    Add files via upload

---
+++ music.js
@@ -0,0 +1,20 @@
+const albums = [
+  {
+    t: "Album One",
+    d: "A short description 1",
+    i: "m/test.png",
+    s: [
+      { t: "N.G.G.Y.U", a: "Rick A", f: "m/rr.mp3", i: "m/test.png" },
+      { t: "Song 2", a: "Artist", f: "m/song2.mp3", i: "m/test.png" }
+    ]
+  },
+  {
+    t: "Album Two",
+    d: "A short description 2",
+    i: "m/test.png",
+    s: [
+      { t: "Song A", a: "a", f: "m/song3.mp3", i: "m/test.png" },
+      { t: "Song B", a: "b", f: "m/song4.mp3", i: "m/test.png" }
+    ]
+  }
+];
