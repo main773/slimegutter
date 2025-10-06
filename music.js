b401d8697eb1d6784af55745d299454ffb78f263
main773 <chatterapp1234@gmail.com>
2025-10-05 21:02:16 -0400

    Update music.js

--- music.js
+++ music.js
@@ -2,18 +2,18 @@ const albums = [
   {
     t: "Popular",
     d: "Slime Gutter's Top 10 Most Popular",
-    i: "sg.webp",
+    i: "tt.png",
     s: [
-      { t: "Slimecraft", a: "E", f: "Slimecraft.mp3", i: "tt.png" },
+      { t: "Slimecraft", a: "E", f: "Slimecraft.mp3", i: "sg.webp" },
       { t: "Slime Gutter", a: "E", f: "Slime Gutter.mp3", i: "sg.webp" },
-      { t: "Trenches", a: "E", f: "Trenches.mp3", i: "tt.png" },
-      { t: "27 shots", a: "E", f: "27 shots.mp3", i: "tt.png" },
-      { t: "philly", a: "E", f: "philly.mp3", i: "tt.png" },
-      { t: "Jugg", a: "E", f: "Jugg.mp3", i: "tt.png" },
-      { t: "chicken jockey", a: "E", f: "chicken jockey.mp3", i: "tt.png" },
-      { t: "war", a: "E", f: "war.mp3", i: "tt.png" },
-      { t: "Massive", a: "E", f: "Massive.mp3", i: "tt.png" },
-      { t: "Bag of Money", a: "E", f: "Bag of Money.mp3", i: "tt.png" }
+      { t: "Trenches", a: "E", f: "Trenches.mp3", i: "sg.webp" },
+      { t: "27 shots", a: "E", f: "27 shots.mp3", i: "sg.webp" },
+      { t: "philly", a: "E", f: "philly.mp3", i: "sg.webp" },
+      { t: "Jugg", a: "E", f: "Jugg.mp3", i: "sg.webp" },
+      { t: "chicken jockey", a: "E", f: "chicken jockey.mp3", i: "sg.webp" },
+      { t: "war", a: "E", f: "war.mp3", i: "sg.webp" },
+      { t: "Massive", a: "E", f: "Massive.mp3", i: "sg.webp" },
+      { t: "Bag of Money", a: "E", f: "Bag of Money.mp3", i: "sg.webp" }
     ]
   },
   {
@@ -26,5 +26,6 @@ const albums = [
     ]
   }
 ];
+
 
 
