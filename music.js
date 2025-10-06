6384b24bf75c395d9a85ebe0f4777f1e39012e4d
main773 <chatterapp1234@gmail.com>
2025-10-05 20:31:56 -0400

    Update music.js

--- music.js
+++ music.js
@@ -1,11 +1,19 @@
 const albums = [
   {
-    t: "Place Holder 1",
-    d: "A short description 1",
-    i: "m/test.png",
+    t: "Popular",
+    d: "Slime Gutter's Top 10 Most Popular",
+    i: "m/sg.png",
     s: [
-      { t: "Test Song 1", a: "Rick A", f: "m/rr.mp3", i: "m/test.png" },
-      { t: "Placeholder A1 no audio", a: "Artist", f: "m/song2.mp3", i: "m/test.png" }
+      { t: "Slimecraft", a: "Slime Gutter", f: "m/slimecraft.mp3", i: "m/sg.png", d: "1:11" },
+      { t: "Slime Gutter", a: "Slime Gutter", f: "m/slime_gutter.mp3", i: "m/sg.png", d: "3:39" },
+      { t: "Trenches", a: "Slime Gutter", f: "m/trenches.mp3", i: "m/sg.png", d: "2:18" },
+      { t: "27 shots", a: "Slime Gutter", f: "m/27_shots.mp3", i: "m/sg.png", d: "1:29" },
+      { t: "philly", a: "Slime Gutter", f: "m/philly.mp3", i: "m/sg.png", d: "1:16" },
+      { t: "Jugg", a: "Slime Gutter", f: "m/jugg.mp3", i: "m/sg.png", d: "1:52" },
+      { t: "chicken jockey", a: "Slime Gutter", f: "m/chicken_jockey.mp3", i: "m/sg.png", d: "1:33" },
+      { t: "war", a: "Slime Gutter", f: "m/war.mp3", i: "m/sg.png" },
+      { t: "Massive", a: "Slime Gutter", f: "m/massive.mp3", i: "m/sg.png", d: "2:34" },
+      { t: "Bag of Money", a: "Slime Gutter", f: "m/bag_of_money.mp3", i: "m/sg.png", d: "4:18" }
     ]
   },
   {
@@ -18,4 +26,3 @@ const albums = [
     ]
   }
 ];
-
