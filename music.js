bf7e44f6a35f929a51edb4aafdf4161e64e92d1e
main773 <chatterapp1234@gmail.com>
2025-10-05 20:55:24 -0400

    Update music.js

--- music.js
+++ music.js
@@ -2,18 +2,18 @@ const albums = [
   {
     t: "Popular",
     d: "Slime Gutter's Top 10 Most Popular",
-    i: "m/sg.webp",
+    i: "sg.webp",
     s: [
-      { t: "Slimecraft", a: "Slime Gutter", f: "m/Slimecraft.mp3", i: "m/sg.webp", d: "1:11" },
-      { t: "Slime Gutter", a: "Slime Gutter", f: "m/Slime Gutter.mp3", i: "m/sg.webp", d: "3:39" },
-      { t: "Trenches", a: "Slime Gutter", f: "m/Trenches.mp3", i: "m/sg.webp", d: "2:18" },
-      { t: "27 shots", a: "Slime Gutter", f: "m/27 shots.mp3", i: "m/sg.webp", d: "1:29" },
-      { t: "philly", a: "Slime Gutter", f: "m/philly.mp3", i: "m/sg.webp", d: "1:16" },
-      { t: "Jugg", a: "Slime Gutter", f: "m/Jugg.mp3", i: "m/sg.webp", d: "1:52" },
-      { t: "chicken jockey", a: "Slime Gutter", f: "m/chicken jockey.mp3", i: "m/sg.webp", d: "1:33" },
-      { t: "war", a: "Slime Gutter", f: "m/war.mp3", i: "m/sg.webp" },
-      { t: "Massive", a: "Slime Gutter", f: "m/Massive.mp3", i: "m/sg.webp", d: "2:34" },
-      { t: "Bag of Money", a: "Slime Gutter", f: "m/Bag of Money.mp3", i: "m/sg.webp", d: "4:18" }
+      { t: "Slimecraft", a: "E", f: "Slimecraft.mp3", i: "tt.png" },
+      { t: "Slime Gutter", a: "E", f: "Slime Gutter.mp3", i: "sg.webp" },
+      { t: "Trenches", a: "E", f: "Trenches.mp3", i: "tt.png" },
+      { t: "27 shots", a: "E", f: "27 shots.mp3", i: "tt.png" },
+      { t: "philly", a: "E", f: "philly.mp3", i: "tt.png" },
+      { t: "Jugg", a: "E", f: "Jugg.mp3", i: "tt.png" },
+      { t: "chicken jockey", a: "E", f: "chicken jockey.mp3", i: "tt.png" },
+      { t: "war", a: "E", f: "war.mp3", i: "tt.png" },
+      { t: "Massive", a: "E", f: "Massive.mp3", i: "tt.png" },
+      { t: "Bag of Money", a: "E", f: "Bag of Money.mp3", i: "tt.png" }
     ]
   },
   {
@@ -21,13 +21,10 @@ const albums = [
     d: "A short description 2",
     i: "test.png",
     s: [
-      { t: "rr", a: "Rick A", f: "m/rr.mp3", i: "test.png" },
-      { t: "Placeholder A no audio", a: "Artist", f: "m/song2.mp3", i: "test.png" },
-      { t: "Placeholder B no audio", a: "Artist", f: "m/song3.mp3", i: "test.png" }
+      { t: "Test Song 1", a: "Rick A", f: "rr.mp3", i: "test.png" },
+      { t: "Placeholder A1 no audio", a: "Artist", f: "song2.mp3", i: "test.png" }
     ]
   }
 ];
-
-
 
 
